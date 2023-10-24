c544906a1bfd528335ff82dfe9e9a53bcadf9d3f
Naveen <nasharma314@gmail.com>
2023-10-24 13:23:27 +0530

    Modified

---
+++ 2. Back End Development/1. Node JS/1.1 Getting Started/node.js
@@ -0,0 +1,20 @@
+/* It's an asynchronous event-driven JavaScript runtime, Node.js is designed to build scalable network applications.
+   
+   This provide JS a runtime environment in our server. 
+*/
+
+/* REPL stands for Read Evaluate Print Loop, 
+   and it is a programming language environment (basically a console window) that takes single expression as user input 
+   and returns the result back to the console after execution. 
+   
+   The REPL session provides a convenient way to quickly test simple JavaScript code.
+*/
+
+/* Write following commands in terminal to getting started with Node.js
+   ~ node -v (To check version of node.js)
+   ~ node (To enter in Node REPL)
+   > .help
+   > .exit (To exit from REPL)
+*/
+
+// We can also write simple js program in node REPL, as shown in image.
